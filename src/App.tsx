4c1148a1424b6ec1ed31ef3f3170e356e30cc0de
Sultan Mukhtarov <suli419956@gmail.com>
2024-03-01 12:49:24 +0600

    initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+    <div>Privet!</div>
+  )
+};
+
+export default App
---
+++ src/app/store.ts
@@ -0,0 +1,10 @@
+import {configureStore} from "@reduxjs/toolkit";
+import rootReducer from '.'
+
+export const store = configureStore({
+    reducer: {
+
+    }
+});
+
+export default store;
